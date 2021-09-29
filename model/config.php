3939466953020379180f8a318edd5f244cf096f7
@LuisArana <disconluis@gmail.com>
2021-09-29 13:16:59 +0200

    feat(root): add config for production

--- model/config.php
+++ model/config.php
@@ -1,5 +1,12 @@
 <?php 
+  //desarrollo
   define("DBHOST", "localhost");
   define("DBUSER", "luichidev");
   define("DBPASS", "admin123");
   define("DBNAME", "vegas_db");
+
+  //produccion
+  // define("DBHOST", "fdb21.awardspace.net");
+  // define("DBUSER", "2754830_vegas");
+  // define("DBPASS", "V3g4s4dm1n123");
+  // define("DBNAME", "2754830_vegas");
